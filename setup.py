d1d63a9de258c300aec769feabbc656a6aeff753
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-12-11 12:01:21 +0900

    fix typo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
-    install_requires=['setuptools, 'numpy'],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Kazuha Mogi',
     maintainer_email='mogi2fruits.kazu@gmail.com',
