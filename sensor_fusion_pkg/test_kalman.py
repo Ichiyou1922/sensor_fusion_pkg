a125ef79608dfb0d04c339a10c7c1fd8dbed65ab
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-12-11 11:19:30 +0900

    test test_kalman.py

---
+++ sensor_fusion_pkg/test_kalman.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_dummy_math():
+    assert 1 + 1 == 2
+
+def test_logic_check():
+    initial_p = 10.0
+    p_predicted = initial_p + q
+    assert p_predicted > initial_p
+
