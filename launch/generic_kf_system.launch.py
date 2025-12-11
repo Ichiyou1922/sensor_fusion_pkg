053cc386c46ba77bb61cf64e851fec64ffb972a8
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-12-12 01:36:45 +0900

    add 4dyaml and launchargument

--- launch/generic_kf_system.launch.py
+++ launch/generic_kf_system.launch.py
@@ -1,51 +1,48 @@
 # SPDX-FileCopyrightText: 2025 Kazuha Mogi <mogi2fruits.kazu@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
-from launch import LaunchDescription
-from launch_ros.actions import Node
-import os
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
     pkg_name = 'sensor_fusion_pkg'
     pkg_share = get_package_share_directory(pkg_name)
 
-    # YAML パラメータへのパス
-    param_file = os.path.join(
-        pkg_share,
-        'config',
-        'generic_kf_1d2sens.yaml'
-    )
+    default_param = os.path.join(
+            pkg_share, 'config', 'generic_kf_1d2sens.yaml'
+            )
 
     return LaunchDescription([
-        # センサ1（精度が高い）
+        DeclareLaunchArgument(
+            'param_file',
+            default_value=default_param,
+            description='Kalman Filter parameter YAML file'
+        ),
+
         Node(
             package=pkg_name,
             executable='noisy_sensor',
             name='sensor_precise',
-            parameters=[{
-                'variance': 1.0,
-                'sensor_id': 1,
-            }],
+            parameters=[{'variance': 1.0, 'sensor_id': 1}],
         ),
 
-        # センサ2（ノイズが大きい）
         Node(
             package=pkg_name,
             executable='noisy_sensor',
             name='sensor_noisy',
-            parameters=[{
-                'variance': 5.0,
-                'sensor_id': 2,
-            }],
+            parameters=[{'variance': 5.0, 'sensor_id': 2}],
         ),
 
-        # 汎用カルマンノード
         Node(
             package=pkg_name,
             executable='generic_kf_node',
             name='generic_kf',
-            parameters=[param_file],
+            parameters=[LaunchConfiguration('param_file')],
         ),
     ])
+
