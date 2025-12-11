0ab8be0606825c86ec1d3a3dbb44ab10f777f58a
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-12-11 16:11:37 +0900

    add kalman_filters.py

--- sensor_fusion_pkg/kalman_filters.py
+++ sensor_fusion_pkg/kalman_filters.py
@@ -1,3 +1,16 @@
 import numpy as np
+from typing import Optional
 
-class KalmanFilter
+class KalmanFilter:
+    def __init__(self, x0, P0, F, Q, H, R):
+        dtype = np.float64
+        x0 = np.asarray(x0, dtype=dtype)
+        P0 = np.asarray(P0, dtype=dtype)
+        F = np.asarray(F, dtype=dtype)
+        Q = np.asarray(Q, dtype=dtype)
+        H = np.asarray(H, dtype=dtype)
+        R = np.asarray(R, dtype=dtype)
+
+        n = x0.shape[0]
+        m = H.shape[0]
+
