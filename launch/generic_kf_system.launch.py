d7fb8fc48d707eab8ad2c9ca58c3c48a4ee6c875
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-12-11 19:12:47 +0900

    test

---
+++ launch/generic_kf_system.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    pkg_name = 'sensor_fusion_pkg'
+    pkg_share = get_package_share_directory(pkg_name)
+
+    # YAML パラメータへのパス
+    param_file = os.path.join(
+        pkg_share,
+        'config',
+        'generic_kf_1d2sens.yaml'
+    )
+
+    return LaunchDescription([
+        # センサ1（精度が高い）
+        Node(
+            package=pkg_name,
+            executable='noisy_sensor',
+            name='sensor_precise',
+            parameters=[{
+                'variance': 1.0,
+                'sensor_id': 1,
+            }],
+        ),
+
+        # センサ2（ノイズが大きい）
+        Node(
+            package=pkg_name,
+            executable='noisy_sensor',
+            name='sensor_noisy',
+            parameters=[{
+                'variance': 5.0,
+                'sensor_id': 2,
+            }],
+        ),
+
+        # 汎用カルマンノード
+        Node(
+            package=pkg_name,
+            executable='generic_kf_node',
+            name='generic_kf',
+            parameters=[param_file],
+        ),
+    ])
--- sensor_fusion_pkg/test_kalman.py
+++ sensor_fusion_pkg/test_kalman.py
@@ -1,10 +1,26 @@
-def test_dummy_math():
-    assert 1 + 1 == 2
+import numpy as np
+from sensor_fusion_pkg.kalman_filters import KalmanFilter
 
 
-def test_logic_check():
-    initial_p = 10.0
-    q = 1.0
-    # 予測ステップのシミュレーション
-    p_predicted = initial_p + q
-    assert p_predicted > initial_p
+# predict(), update()のテスト 
+def test_kalman_1d_no_noise_2():
+    # 1次元の理想的なモデル
+    x0 = np.array([0.0])        # 初期状態 x0 = 0
+    P0 = np.array([[1.0]])      # 初期共分散 P0 = 1
+    F = np.array([[1.0]])      # 定常モデル x_k = x_{k-1}
+    Q = np.array([[0.0]])      # プロセスノイズなし
+    H = np.array([[1.0]])      # 状態をそのまま観測
+    R = np.array([[0.0]])      # 観測ノイズなし
+
+    kf = KalmanFilter(x0, P0, F, Q, H, R)
+
+    z = np.array([10.0])        # 観測値 z = 10
+
+    # 予測＋更新
+    kf.predict()
+    kf.update(z)
+
+    # 理論的に x は 10，P は 0 になっていなければならない
+    assert np.allclose(kf.x, np.array([10.0]))
+    assert np.allclose(kf.P, np.array([[0.0]]))
+
