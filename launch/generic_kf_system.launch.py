d905086ffe3b420bfcc7eb06bec98c2b4af6827e
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2026-01-04 17:23:33 +0900

    add launch.condition module

--- launch/generic_kf_system.launch.py
+++ launch/generic_kf_system.launch.py
@@ -6,6 +6,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.condition import IfCondition
 import os
 
 
